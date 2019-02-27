d69d4da166c2f31a7335b529a3eed5ea807a6ca5
noneofthelis <alisa.snag@gmail.com>
2019-02-27 15:43:13 +0100

    refactored make-filter function

--- src/make-filter.js
+++ src/make-filter.js
@@ -11,12 +11,16 @@ export default (name, number, isChecked) => {
   const template = document.querySelector(`#filter-template`).content;
   const fragment = document.createDocumentFragment();
   const element = template.cloneNode(true);
+  const label = element.querySelector(`label`);
+  const span = element.querySelector(`span`);
+
   element.id = `filter__${name}`;
-  element.querySelector(`label`).setAttribute(`for`, `filter__${name}`);
-  element.querySelector(`label`).textContent = name;
-  element.querySelector(`span`).classList.add(`filter__${name}-count`);
-  element.querySelector(`label`).textContent = number;
+  label.setAttribute(`for`, `filter__${name}`);
+  label.textContent = name;
+  span.classList.add(`filter__${name}-count`);
+  span.textContent = number;
   element.setAttribute(`checked`, number ? isChecked : `disabled`);
   fragment.appendChild(element);
+
   return fragment;
 };
